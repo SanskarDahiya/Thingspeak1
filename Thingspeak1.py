5a5b8e977d9aaa7a6832cb9c3de2fdd942e009db
Sanskar Dahiya <44332197+SanskarDahiya@users.noreply.github.com>
2019-03-21 20:26:45 +0530

    Add files via upload
    Contain 2 functions to upload and retrive data

---
+++ Thingspeak1.py
@@ -0,0 +1,48 @@
+#getting data from thingspeak
+#=================================
+import json
+from urllib.request import urlopen
+"""
+chID  = Channel Id
+api   = read api key
+"""
+def retrivedata(chID,api):
+    try:
+        data = "https://api.thingspeak.com/channels/"+chID+"/feeds.json?api_key="+api+"&results=1"
+        data = urlopen(data)
+        data = json.loads(data.read())
+        #get the FEED only
+        data = data['feeds']
+        #bcoz data is at 0 index
+        data = data[0] #give a dict
+        #retriving the data
+        field=list()
+        for x in range(len(data)-2):
+            field.append(data['field'+str(x+1)])
+        return(field)
+    except Exception as e:
+        print("Error Found\n    ",e)
+        return None
+
+"""
+api = Write api key
+n = total number of fields
+
+
+"""
+
+def uploaddata(api,n,*data):
+    try:
+        url = "https://api.thingspeak.com/update?api_key="+api
+        for z in range(n):
+            url+="&field"+str(z+1)+"="+str(data[z])
+        data = urlopen(url)
+        print(url)
+        data = json.loads(data.read())
+        if(data > 0):
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("Error Found\n    ",e)
+        return False
